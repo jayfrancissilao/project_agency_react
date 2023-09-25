e5824058039897a6342758278e1c475d4575b93d
Jay Silao <jayfrancissilao12@gmail.com>
2023-09-26 00:11:33 +0800

    logo na muna sa ngayon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,10 @@
+/* eslint-disable no-unused-vars */
+// import logo
+import Logo from "/src/assets/Logo/logo.jpg";
+import React, { useEffect, useState } from "react";
+
 const Navbar = () => {
+  // here the code
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
 
@@ -26,13 +32,16 @@ const Navbar = () => {
     { link: "Testimonials", path: "testimonials" },
     { link: "FAQ", path: "faq" },
   ];
-
   return (
     <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
       <nav>
         <div>
-          <a href="">
-            <img src="" alt="" />
+          <a
+            href=""
+            className="text-2xl font-semibold flex items-center space-x-3"
+          >
+            <img src={Logo} alt="" className="w-10 inline-block items-center" />
+            <span className="text-[#263238]">Roment</span>
           </a>
         </div>
       </nav>
